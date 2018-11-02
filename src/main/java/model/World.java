7a1506d12bc7be4ba700c9c76e53913df37cd44b
Sean Eikenberg <sean_eikenberg@apple.com>
2018-11-01 21:46:32 -0700

    Finish the world.

--- src/main/java/model/World.java
+++ src/main/java/model/World.java
@@ -388,6 +388,49 @@ public class World {
                         .position(Position.builder().horizontal(17).vertical(9).build())
                         .build()
         );
-
+        buildingBlocks.add(
+                BuildingBlock.builder()
+                        .type(BuildingBlock.Type.Wall)
+                        .image(new Image("images/Sprite 045 - Wall.png"))
+                        .position(Position.builder().horizontal(0).vertical(11).build())
+                        .build()
+        );
+        buildingBlocks.add(
+                BuildingBlock.builder()
+                        .type(BuildingBlock.Type.Wall)
+                        .image(new Image("images/Sprite 046 - Wall.png"))
+                        .position(Position.builder().horizontal(1).vertical(11).build())
+                        .position(Position.builder().horizontal(3).vertical(11).build())
+                        .position(Position.builder().horizontal(5).vertical(11).build())
+                        .position(Position.builder().horizontal(7).vertical(11).build())
+                        .position(Position.builder().horizontal(9).vertical(11).build())
+                        .position(Position.builder().horizontal(11).vertical(11).build())
+                        .position(Position.builder().horizontal(13).vertical(11).build())
+                        .position(Position.builder().horizontal(15).vertical(11).build())
+                        .position(Position.builder().horizontal(17).vertical(11).build())
+                        .build()
+        );
+        buildingBlocks.add(
+                BuildingBlock.builder()
+                        .type(BuildingBlock.Type.Wall)
+                        .image(new Image("images/Sprite 047 - Wall.png"))
+                        .position(Position.builder().horizontal(2).vertical(11).build())
+                        .position(Position.builder().horizontal(4).vertical(11).build())
+                        .position(Position.builder().horizontal(6).vertical(11).build())
+                        .position(Position.builder().horizontal(8).vertical(11).build())
+                        .position(Position.builder().horizontal(10).vertical(11).build())
+                        .position(Position.builder().horizontal(12).vertical(11).build())
+                        .position(Position.builder().horizontal(14).vertical(11).build())
+                        .position(Position.builder().horizontal(16).vertical(11).build())
+                        .position(Position.builder().horizontal(18).vertical(11).build())
+                        .build()
+        );
+        buildingBlocks.add(
+                BuildingBlock.builder()
+                        .type(BuildingBlock.Type.Wall)
+                        .image(new Image("images/Sprite 048 - Wall.png"))
+                        .position(Position.builder().horizontal(19).vertical(11).build())
+                        .build()
+        );
     }
 }
