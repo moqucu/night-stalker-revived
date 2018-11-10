e2319fd47dad5bc529d51303a1ea96cc7dae2c59
Sean Eikenberg <sean_eikenberg@apple.com>
2018-11-10 11:33:10 -0800

    Implement first simple random path logic.

--- src/main/java/model/Bat.java
+++ src/main/java/model/Bat.java
@@ -6,14 +6,30 @@ import javafx.scene.input.KeyCode;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
+import static model.Bat.Direction.*;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Bat extends AnimatedSprite implements Updatable {
 
+    enum Direction {
+        Up, Right, Down, Left;
+
+        private static final List<Direction> VALUES =
+                Collections.unmodifiableList(Arrays.asList(values()));
+        private static final int SIZE = VALUES.size();
+        private static final Random RANDOM = new Random();
+
+        public static Direction randomDirection()  {
+            return VALUES.get(RANDOM.nextInt(SIZE));
+        }
+    }
+
     private boolean awake = false;
+
+    private Direction direction = Right;
 
     public Bat() {
         super(Coordinates.builder().x(17 * WIDTH - WIDTH / 2).y(3 * HEIGHT).build());
@@ -49,31 +65,49 @@ public class Bat extends AnimatedSprite implements Updatable {
         if (!awake)
             return;
 
-        TransparentSprite transparentSprite
-                = new TransparentSprite(getCurrentCoordinates().getX(), getCurrentCoordinates().getY());
+        boolean collision = checkDirectionForCollision(getCurrentCoordinates(), direction, sprites);
 
-        if (input.size() > 0 && input.contains(KeyCode.RIGHT))
-            transparentSprite.getCurrentCoordinates().setX(getCurrentCoordinates().getX() + 4);
-        if (input.size() > 0 && input.contains(KeyCode.LEFT))
-            transparentSprite.getCurrentCoordinates().setX(getCurrentCoordinates().getX() - 4);
-        if (input.size() > 0 && input.contains(KeyCode.UP))
-            transparentSprite.getCurrentCoordinates().setY(getCurrentCoordinates().getY() - 4);
-        if (input.size() > 0 && input.contains(KeyCode.DOWN))
-            transparentSprite.getCurrentCoordinates().setY(getCurrentCoordinates().getY() + 4);
+        if (collision) {
 
-        boolean anyCollision = sprites
-                .stream()
-                .anyMatch(sprite -> sprite.intersects(transparentSprite) & !sprite.equals(this));
+            Direction randomDirection = Direction.randomDirection();
+            while (checkDirectionForCollision(getCurrentCoordinates(), randomDirection, sprites)) {
+                randomDirection = Direction.randomDirection();
+            }
+            direction = randomDirection;
 
-        if (!anyCollision) {
-            setCurrentCoordinates(
-                    Coordinates.builder()
-                            .x(transparentSprite.getCurrentCoordinates().getX())
-                            .y(transparentSprite.getCurrentCoordinates().getY())
-                            .build()
-            );
         }
 
+        advanceCoordinatesByDirection(getCurrentCoordinates(), direction);
+    }
 
+    private boolean checkDirectionForCollision(Coordinates coordinates, Direction direction, List<Sprite> sprites) {
+
+        TransparentSprite transparentSprite
+                = new TransparentSprite(coordinates.getX(), coordinates.getY());
+
+        advanceCoordinatesByDirection(transparentSprite.getCurrentCoordinates(), direction);
+
+        return sprites
+                .stream()
+                .anyMatch(sprite -> sprite.intersects(transparentSprite) & !sprite.equals(this));
+    }
+
+    private void advanceCoordinatesByDirection(Coordinates coordinates, Direction direction) {
+
+        switch (direction) {
+
+            case Right:
+                coordinates.setX(getCurrentCoordinates().getX() + 4);
+                break;
+            case Down:
+                coordinates.setY(getCurrentCoordinates().getY() + 4);
+                break;
+            case Left:
+                coordinates.setX(getCurrentCoordinates().getX() - 4);
+                break;
+            case Up:
+                coordinates.setY(getCurrentCoordinates().getY() - 4);
+                break;
+        }
     }
 }
