308e9fa9815eec28f3e04d070dc8cbad987d1ca8
moqucu <Drox6nuC4Ip0dA>
2019-02-15 20:39:54 -0800

    Finish with building the maze.

--- src/main/java/org/moqucu/games/nightstalker/view/immovable/Bunker.java
+++ src/main/java/org/moqucu/games/nightstalker/view/immovable/Bunker.java
@@ -1,10 +1,16 @@
 package org.moqucu.games.nightstalker.view.immovable;
 
+import javafx.geometry.Point2D;
 import javafx.scene.image.Image;
 import org.moqucu.games.nightstalker.view.Sprite;
 import org.moqucu.games.nightstalker.view.movable.NightStalker;
 
 public class Bunker extends Sprite {
+
+    public Bunker() {
+
+        super(Point2D.ZERO);
+    }
 
     public Bunker(Image image, Position initialPosition) {
 
--- src/main/java/org/moqucu/games/nightstalker/view/immovable/HalfSolidBunker.java
+++ src/main/java/org/moqucu/games/nightstalker/view/immovable/HalfSolidBunker.java
@@ -2,6 +2,7 @@ package org.moqucu.games.nightstalker.view.immovable;
 
 import javafx.geometry.BoundingBox;
 import javafx.geometry.Bounds;
+import javafx.geometry.Point2D;
 import javafx.scene.image.Image;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,6 +11,11 @@ import org.moqucu.games.nightstalker.view.Sprite;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class HalfSolidBunker extends Sprite {
+
+    public HalfSolidBunker() {
+
+        super(Point2D.ZERO);
+    }
 
     public HalfSolidBunker(Image image, Position initialPosition) {
 
