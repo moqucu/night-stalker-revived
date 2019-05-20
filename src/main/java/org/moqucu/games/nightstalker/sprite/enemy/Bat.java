9307b7a824d421c22ba35a5510bf5a5d85457820
moqucu <sean.eikenberg@outlook.com>
2019-05-19 21:12:28 -0700

    Make bats hittable by bullets.

--- src/main/java/org/moqucu/games/nightstalker/sprite/enemy/Bat.java
+++ src/main/java/org/moqucu/games/nightstalker/sprite/enemy/Bat.java
@@ -6,7 +6,10 @@ import lombok.EqualsAndHashCode;
 import lombok.SneakyThrows;
 import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
+import org.moqucu.games.nightstalker.sprite.AnimatedSprite;
+import org.moqucu.games.nightstalker.sprite.Hittable;
 import org.moqucu.games.nightstalker.sprite.SleepingSprite;
+import org.moqucu.games.nightstalker.sprite.object.Bullet;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineBuilder;
@@ -22,13 +25,21 @@ import static org.moqucu.games.nightstalker.NightStalkerRevived.translate;
 @SuppressWarnings("unused")
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class Bat extends SleepingSprite {
+public class Bat extends SleepingSprite implements Hittable {
 
-    private enum States {asleep, awake, moving}
+    private enum States {Asleep, Awake, Moving, Dying}
 
-    private enum Events {wakeUp, move, stop}
+    private enum Events {wakeUp, move, stop, die}
 
     private StateMachine<States, Events> stateMachine;
+
+    private Map<States, Indices> frameBoundaries = Map.of(
+            States.Asleep, Indices.builder().lower(0).upper(0).build(),
+            States.Awake, Indices.builder().lower(0).upper(0).build(),
+            States.Moving, Indices.builder().lower(1).upper(5).build(),
+            States.Dying, Indices.builder().lower(6).upper(9).build()
+    );
+
 
     private StateMachineListener<States, Events> stateMachineListener
             = new StateMachineListenerAdapter<>() {
@@ -36,9 +47,17 @@ public class Bat extends SleepingSprite {
         @Override
         public void transitionEnded(org.springframework.statemachine.transition.Transition<States, Events> transition) {
 
-            if (transition.getTarget().getId().equals(States.awake))
-                stateMachine.sendEvent(Events.move);
-        }
+            log.debug("State changed to {}", transition.getTarget().getId());
+
+            setFrameIndices(frameBoundaries.get(transition.getTarget().getId()));
+
+            switch (transition.getTarget().getId()) {
+
+                case Awake:
+                    stateMachine.sendEvent(Events.move);
+                    break;
+             }
+       }
     };
 
     public Bat() {
@@ -46,7 +65,6 @@ public class Bat extends SleepingSprite {
         super();
 
         setImage(new Image(translate("images/bat.png")));
-        setFrameIndices(Indices.builder().lower(1).upper(5).build());
 
         sleepTimeInMillisProperty().addListener((observableValue, number, t1) -> {
 
@@ -79,31 +97,36 @@ public class Bat extends SleepingSprite {
 
         builder.configureStates()
                 .withStates()
-                .initial(States.asleep)
+                .initial(States.Asleep)
                 .states(EnumSet.allOf(States.class));
 
         builder.configureTransitions()
                 .withInternal()
-                .source(States.asleep)
+                .source(States.Asleep)
                 .action(this::timeToWakeUp)
                 .timerOnce(getSleepTimeInMillis())
                 .and()
                 .withExternal()
-                .source(States.asleep)
+                .source(States.Asleep)
                 .action(this::wokeUp)
-                .target(States.awake)
+                .target(States.Awake)
                 .event(Events.wakeUp)
                 .and()
                 .withExternal()
-                .source(States.awake)
-                .target(States.moving)
+                .source(States.Awake)
+                .target(States.Moving)
                 .action(this::startedToMove)
                 .event(Events.move)
                 .and()
                 .withExternal()
-                .source(States.moving)
-                .target(States.awake)
-                .event(Events.stop);
+                .source(States.Moving)
+                .target(States.Awake)
+                .event(Events.stop)
+                .and()
+                .withExternal()
+                .source(States.Moving)
+                .target(States.Dying)
+                .event(Events.die);
 
         return builder.build();
     }
@@ -125,5 +148,20 @@ public class Bat extends SleepingSprite {
         log.debug("startedToMove: {}", stateContext);
         computeNextMoveAnimationBasedOnRandomDirection();
         moveMeFromStart();
+    }
+
+    @Override
+    public void detectCollision(Set<AnimatedSprite> nearbySprites) {
+
+        nearbySprites.forEach(animatedSprite -> {
+
+            if ((animatedSprite instanceof Bullet)
+                    && this.getBoundsInParent().intersects(animatedSprite.getBoundsInParent())) {
+
+                log.debug("Hit by bullet, dying.");
+                stopMovingMe();
+                stateMachine.sendEvent(Events.die);
+            }
+        });
     }
 }
