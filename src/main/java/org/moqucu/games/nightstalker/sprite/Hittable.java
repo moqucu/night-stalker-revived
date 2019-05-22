f6d1c4d0c604e62b18b6bb092ba8f0ca867d21a5
moqucu <sean.eikenberg@outlook.com>
2019-05-21 21:03:34 -0700

    Comment some of the functions.

--- src/main/java/org/moqucu/games/nightstalker/sprite/Hittable.java
+++ src/main/java/org/moqucu/games/nightstalker/sprite/Hittable.java
@@ -2,7 +2,16 @@ package org.moqucu.games.nightstalker.sprite;
 
 import java.util.Set;
 
+/**
+ * When classes want to indicate that they are 'hittable' by other classes, they shall implement this interface.
+ */
 public interface Hittable {
 
+    /**
+     * This method needs to be implemented for detecting collisions with other objects. Right now, the actual collision
+     * detection has to happen de-centrally as part of every class that implements this method.
+     *
+     * @param nearbySprites The set of sprites that is handed into this method and could cause a collision.
+     */
     void detectCollision(Set<AnimatedSprite> nearbySprites);
 }
--- src/main/java/org/moqucu/games/nightstalker/sprite/Sprite.java
+++ src/main/java/org/moqucu/games/nightstalker/sprite/Sprite.java
@@ -13,6 +13,9 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import org.moqucu.games.nightstalker.view.Maze;
 
+/**
+ * Represents a basic sprite class that specializes ImageView.
+ */
 @Data
 @Log4j2
 @SuppressWarnings("unused")
@@ -23,10 +26,38 @@ public abstract class Sprite extends ImageView {
 
     public static final double HEIGHT = 32.0;
 
+    /**
+     * Exception is thrown whenever expected Maze object couldn't be found in sprite's parent hierarchy.
+     */
+    public class MazeNotFoundException extends RuntimeException {
+
+        MazeNotFoundException() {
+
+            super("No Maze object found as part of sprite's parent hierarchy!");
+        }
+    }
+
+    /**
+     * Exception is thrown whenever expected the index for accessing a sprite sheet's cell is < 0 or > 239.
+     */
+    public class SpriteSheetIndexOutOfBoundsException extends RuntimeException {
+
+        SpriteSheetIndexOutOfBoundsException() {
+
+            super("Index for accessing the sprite sheet has to be between 0 and 239!");
+        }
+    }
+
+    /**
+     * Represents the cell index of the image tile that shall be used for rendering purposes. Default value is 0.
+     */
     @Getter(AccessLevel.NONE)
     @Setter(AccessLevel.NONE)
     private IntegerProperty stillImageIndex = new SimpleIntegerProperty(0);
 
+    /**
+     * Constructor that sets stillImageIndex based on standard initialization value (= 0).
+     */
     public Sprite() {
 
         super();
@@ -38,14 +69,16 @@ public abstract class Sprite extends ImageView {
      * between 0 and 239 which represents all possible 240 tiles in the 20 x 12 matrix.
      *
      * @param index Index of the tile to be retrieved as a rectangular viewport.
+     *
      * @return Rectangle that represents the boundaries of the accessed tile.
+     *
      * @throws RuntimeException if index is below 0 or above 239.
      */
     @SuppressWarnings("WeakerAccess")
     protected Rectangle2D getViewport(int index) {
 
         if (index < 0 || index > 239)
-            throw new RuntimeException("Frame index " + index + "is out of bounds, has to be between 0 and 239!");
+            throw new SpriteSheetIndexOutOfBoundsException();
 
         int horizontalIndex = index % 20;
         int verticalIndex = index / 20;
@@ -53,11 +86,21 @@ public abstract class Sprite extends ImageView {
         return new Rectangle2D(horizontalIndex * WIDTH, verticalIndex * HEIGHT, WIDTH, HEIGHT);
     }
 
+    /**
+     * Returns current value of stillImageIndex.
+     *
+     * @return current value of stillImageIndex.
+     */
     public int getStillImageIndex() {
 
         return stillImageIndex.get();
     }
 
+    /**
+     * Sets the value of stillImageIndex.
+     *
+     * @param index future value of stillImageIndex.
+     */
     @SuppressWarnings("WeakerAccess")
     public void setStillImageIndex(int index) {
 
@@ -65,11 +108,23 @@ public abstract class Sprite extends ImageView {
         setViewport(getViewport(stillImageIndex.get()));
     }
 
+    /**
+     * Returns the property object for the stillImageIndex.
+     *
+     * @return property object for the stillImageIndex.
+     */
     public IntegerProperty stillImageIndexProperty() {
 
         return stillImageIndex;
     }
 
+    /**
+     * Traverses the parent objects of this sprite and returns the Maze object, if found.
+     *
+     * @return Maze object if found.
+     *
+     * @throws MazeNotFoundException if no Maze object could be found as part of parent hierarchy traversal.
+     */
     protected Maze getMaze() {
 
         boolean foundParentOrNothing = false;
@@ -81,6 +136,9 @@ public abstract class Sprite extends ImageView {
             else
                 parent = parent.getParent();
         }
+
+        if (parent == null)
+            throw new MazeNotFoundException();
 
         return (Maze)parent;
     }
