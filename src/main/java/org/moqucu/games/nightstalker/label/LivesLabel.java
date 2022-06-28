b02e4db642d4a94e7a6e2db417f5ab02531441aa
Bradley-Chang <chang713@purdue.edu>
2022-06-28 11:12:39 -0700

    un-break lives label transitions

--- src/main/java/org/moqucu/games/nightstalker/label/LivesLabel.java
+++ src/main/java/org/moqucu/games/nightstalker/label/LivesLabel.java
@@ -1,6 +1,6 @@
 package org.moqucu.games.nightstalker.label;
 
-import javafx.animation.FadeTransition;
+import javafx.animation.*;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.text.Font;
@@ -15,36 +15,23 @@ public class LivesLabel extends Text {
 
     private FadeTransition fadeTransition;
 
-    private FadeTransition fadeTransition2;
-
-    private FadeTransition fadeTransition3;
 
     public LivesLabel() {
         InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream("fonts/intellect.ttf");
         setFont(Font.loadFont(stream, 40.0));
 
-        fadeTransition = new FadeTransition(Duration.millis(100), this);
+        fadeTransition = new FadeTransition(Duration.millis(2000), this);
         fadeTransition.setFromValue(0.0);
         fadeTransition.setToValue(1.0);
         fadeTransition.setCycleCount(2);
+        fadeTransition.setAutoReverse(true);
 
-        fadeTransition2 = new FadeTransition(Duration.millis(2000), this);
-        fadeTransition2.setFromValue(1.0);
-        fadeTransition2.setToValue(1.0);
-        fadeTransition2.setCycleCount(2);
-
-        fadeTransition3 = new FadeTransition(Duration.millis(100), this);
-        fadeTransition3.setFromValue(1.0);
-        fadeTransition3.setToValue(0.0);
-        fadeTransition3.setCycleCount(2);
 
         lives.addListener((observable, oldValue, newValue) -> {
             if (!newValue.equals(oldValue)) {
 
                 setText(newValue.toString());
                 fadeTransition.play();
-                fadeTransition2.play();
-                fadeTransition3.play();
             }
         });
     }
