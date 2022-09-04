dfc49ce5400506dda4bfd74d6a8b9fd65d509c08
S. Eikenberg <moqucu@icloud.com>
2022-09-03 20:31:31 -0700

    Enhance game object with more properties and behavior.

--- src/main/java/org/moqucu/games/nightstalker/model/GameObject.java
+++ src/main/java/org/moqucu/games/nightstalker/model/GameObject.java
@@ -1,10 +1,21 @@
 package org.moqucu.games.nightstalker.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 public abstract class GameObject {
+
+    public static class PreconditionNotMetForMakingObjectVisibleException extends RuntimeException {
+
+        PreconditionNotMetForMakingObjectVisibleException(String message) {
+
+            super(message);
+        }
+    }
 
     @Getter
     private final AbsolutePosition absolutePosition = new AbsolutePosition();
@@ -17,4 +28,31 @@ public abstract class GameObject {
 
     @Getter
     private final double height = 32.0;
+
+    @Getter
+    @Setter
+    private String imageMapFileName = "";
+
+    @Getter
+    @Setter
+    private int initialImageIndex = -1;
+
+    @Getter
+    private boolean visible = false;
+
+    public void setVisible(boolean visible) {
+
+        try (InputStream inputStream = getClass().getResourceAsStream(imageMapFileName)) {
+
+            if (inputStream == null)
+                throw new PreconditionNotMetForMakingObjectVisibleException("Image map file name not correctly set!");
+            else if (initialImageIndex == -1)
+                throw new PreconditionNotMetForMakingObjectVisibleException("Initial image index not correctly set!");
+            else
+                this.visible = visible;
+
+        } catch (IOException ioException) {
+            throw new PreconditionNotMetForMakingObjectVisibleException("Image map file name not correctly set!");
+        }
+    }
 }
--- src/test/java/org/moqucu/games/nightstalker/model/test/GameObjectTest.java
+++ src/test/java/org/moqucu/games/nightstalker/model/test/GameObjectTest.java
@@ -1,14 +1,14 @@
 package org.moqucu.games.nightstalker.model.test;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.moqucu.games.nightstalker.model.GameObject;
 
 import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.isA;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GameObjectTest {
 
@@ -55,5 +55,63 @@ public class GameObjectTest {
     public void heightIsThirtyTwoPixelsHigh() {
 
         assertThat(genericGameObject.getHeight(), is(32.0));
+    }
+
+    @Test
+    public void hasImageMapFileNameProperty() {
+
+        assertThat(genericGameObject, hasProperty("imageMapFileName"));
+    }
+
+    @Test
+    public void imageMapFileNamePropertyOfTypeString() {
+
+        assertThat(genericGameObject.getImageMapFileName(), instanceOf(String.class));
+    }
+
+    @Test
+    public void hasInitialImageIndexProperty() {
+
+        assertThat(genericGameObject, hasProperty("initialImageIndex"));
+    }
+
+    @Test
+    public void initialImageIndexPropertyOfTypeInteger() {
+
+        assertThat(genericGameObject.getInitialImageIndex(), instanceOf(Integer.class));
+    }
+
+    @Test
+    public void hasVisibleProperty() {
+
+        assertThat(genericGameObject, hasProperty("visible"));
+    }
+
+    @Test
+    public void visiblePropertyOfTypeBoolean() {
+
+        assertThat(genericGameObject.isVisible(), instanceOf(Boolean.class));
+    }
+
+    @Test
+    @DisplayName("Cannot set game object visible without correctly set image map and initial index.")
+    public void settingVisibleToTrueOnlyPossibleWithRightConditions() {
+
+        genericGameObject.setImageMapFileName("");
+        genericGameObject.setInitialImageIndex(-1);
+        assertThrows(
+                GameObject.PreconditionNotMetForMakingObjectVisibleException.class,
+                () -> genericGameObject.setVisible(true)
+        );
+    }
+
+    @Test
+    @DisplayName("Given correct context, setting game object to true shall work.")
+    public void givenCorrectContextSettingVisibleToTrueWorks() {
+
+        genericGameObject.setImageMapFileName("test_image.gif");
+        genericGameObject.setInitialImageIndex(0);
+        genericGameObject.setVisible(true);
+        assertThat(genericGameObject.isVisible(), is(true));
     }
 }
