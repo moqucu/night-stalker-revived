e7498add927c2fc8c893c2a16f1b2877972f801f
S. Eikenberg <moqucu@icloud.com>
2022-09-27 21:13:55 -0700

    Implement random algorithm.

---
+++ src/test/java/org/moqucu/games/nightstalker/model/test/RandomTest.java
@@ -0,0 +1,126 @@
+package org.moqucu.games.nightstalker.model.test;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.moqucu.games.nightstalker.model.*;
+
+import java.io.InputStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+public class RandomTest {
+
+    private final OuterRing outerRing = new OuterRing();
+    private static final String MAZE_GRAPH_NAME = "MazeGraphTest.json";
+    private final AbsMazeGraph absMazeGraph;
+
+    @SneakyThrows
+    public RandomTest() {
+
+        try (InputStream inputStream = getClass().getResourceAsStream(MAZE_GRAPH_NAME)) {
+
+            final MazeGraphV2 mazeGraph = new MazeGraphV2();
+            mazeGraph.loadFromJson(inputStream);
+            absMazeGraph = new AbsMazeGraph(mazeGraph);
+
+        }
+    }
+
+    @Test
+    public void from_32_00_Up_To_32_32_Down() {
+
+        assertThat(
+                outerRing.getNextAbsPos(
+                        absMazeGraph,
+                        new AbsPosAndDirection(new AbsolutePosition(32.0, 0.0), Direction.Up)
+                ),
+                is(new AbsPosAndDirection(new AbsolutePosition(32.0, 32.0), Direction.Down))
+        );
+    }
+
+    @Test
+    public void from_32_32_Down_Not_To_32_00_Up() {
+
+        assertThat(
+                outerRing.getNextAbsPos(
+                        absMazeGraph,
+                        new AbsPosAndDirection(new AbsolutePosition(32.0, 32.0), Direction.Down)
+                ),
+                is(not(new AbsPosAndDirection(new AbsolutePosition(32.0, 0.0), Direction.Up)))
+        );
+    }
+
+    @Test
+    public void from_32_32_Up_Not_To_32_64_Down() {
+
+        assertThat(
+                outerRing.getNextAbsPos(
+                        absMazeGraph,
+                        new AbsPosAndDirection(new AbsolutePosition(32.0, 32.0), Direction.Up)
+                ),
+                is(not(new AbsPosAndDirection(new AbsolutePosition(32.0, 64.0), Direction.Down)))
+        );
+    }
+
+    @Test
+    public void from_32_32_Left_Not_To_64_32_Right() {
+
+        assertThat(
+                outerRing.getNextAbsPos(
+                        absMazeGraph,
+                        new AbsPosAndDirection(new AbsolutePosition(32.0, 32.0), Direction.Left)
+                ),
+                is(not(new AbsPosAndDirection(new AbsolutePosition(64.0, 32.0), Direction.Right)))
+        );
+    }
+
+    @Test
+    public void from_32_32_Right_Not_To_00_32_Left() {
+
+        assertThat(
+                outerRing.getNextAbsPos(
+                        absMazeGraph,
+                        new AbsPosAndDirection(new AbsolutePosition(32.0, 32.0), Direction.Right)
+                ),
+                is(not(new AbsPosAndDirection(new AbsolutePosition(0.0, 32.0), Direction.Left)))
+        );
+    }
+
+    @Test
+    public void from_00_32_Left_To_32_32_Right() {
+
+        assertThat(
+                outerRing.getNextAbsPos(
+                        absMazeGraph,
+                        new AbsPosAndDirection(new AbsolutePosition(0.0, 32.0), Direction.Left)
+                ),
+                is(new AbsPosAndDirection(new AbsolutePosition(32.0, 32.0), Direction.Right))
+        );
+    }
+
+    @Test
+    public void from_32_64_Down_To_32_32_Up() {
+
+        assertThat(
+                outerRing.getNextAbsPos(
+                        absMazeGraph,
+                        new AbsPosAndDirection(new AbsolutePosition(32.0, 64.0), Direction.Down)
+                ),
+                is(new AbsPosAndDirection(new AbsolutePosition(32.0, 32.0), Direction.Up))
+        );
+    }
+
+    @Test
+    public void from_96_32_Right_To_64_32_Left() {
+
+        assertThat(
+                outerRing.getNextAbsPos(
+                        absMazeGraph,
+                        new AbsPosAndDirection(new AbsolutePosition(96.0, 32.0), Direction.Right)
+                ),
+                is(new AbsPosAndDirection(new AbsolutePosition(64.0, 32.0), Direction.Left))
+        );
+    }
+}
